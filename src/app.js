8d12ef363aa8798f19ebe37009dd07ad1a698b48
Shaelyn26 <shaelynb0405@gmail.com>
2025-02-19 00:48:07 +0000

    red colors work

--- src/app.js
+++ src/app.js
@@ -7,26 +7,35 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   let suitArray = ["♦", "♥", "♠", "♣"];
-  let cardArray = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "A", "Q", "K"];
+  let cardArray = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "A", "Q", "K", "J"];
                 //  0,    1,  2,   3,   4,   5,   6,   7,    8,   9,   10,  11,
   
   let suitIndex = Math.floor(Math.random() * 3);
   let randomSuit = suitArray[suitIndex]
-  console.log(suitArray[suitIndex]); 
+   console.log(suitArray[suitIndex]); 
   
-  let cardNumber = Math.floor(Math.random() * 12);
+  let cardNumber = Math.floor(Math.random() * 13);
   let randomCard = cardArray[cardNumber]; 
-  console.log(cardArray[cardNumber]);
+   console.log(cardArray[cardNumber]);
 
 
     let topSuit = document.querySelector(".top-suit") 
-     topSuit.innerHTML = randomSuit;
-     topSuit[0].style.color = "red";
-    console.log("topSuit",topSuit);
+     topSuit.innerHTML = randomSuit; 
+     console.log("topSuit",topSuit);
+     if (topSuit.innerHTML === "♥"|| topSuit.innerHTML === "♦") {
+      topSuit.style.color = "red";
+     } else {
+      topSuit.style.color = "black";
+     }
 
     let bottomSuit = document.querySelector(".bottom-suit")
       bottomSuit.innerHTML = randomSuit;
       console.log("bottomSuit",bottomSuit);
+      if (bottomSuit.innerHTML === "♥"|| bottomSuit.innerHTML === "♦") {
+        bottomSuit.style.color = "red";
+       } else {
+        bottomSuit.style.color = "black";
+       }
 
       let number = document.querySelector(".number")  
         number.innerHTML = randomCard;
